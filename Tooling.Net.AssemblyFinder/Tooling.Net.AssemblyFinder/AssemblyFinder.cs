b4ce6ef9c45854ada9db78cd8c5ae9a84f0bbfbd
MACK Mathieu <mathieu.mack@econocom.com>
2018-01-04 22:04:03 +0100

    Do not work yet, there is a last fix to do : FindInterfaces()

---
+++ Tooling.Net.AssemblyFinder/Tooling.Net.AssemblyFinder/AssemblyFinder.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Tooling.Net.AssemblyFinder
+{
+    public class AssemblyFinder
+    {
+        #region Fields
+
+        private readonly bool loadAppDomainAssemblies = true;
+        private readonly string assemblySkipLoadingPattern = "^System|^mscorlib|^Microsoft";
+        private readonly string assemblyRestrictToLoadingPattern = ".*";
+
+        /// <summary>
+        /// Caches attributed assembly information so they don't have to be re-read
+        /// </summary>
+        private readonly List<AttributedAssembly> _attributedAssemblies = new List<AttributedAssembly>();
+        /// <summary>
+        /// Caches the assembly attributes that have been searched for
+        /// </summary>
+        private readonly List<Type> _assemblyAttributesSearched = new List<Type>();
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>Gets or sets assemblies loaded a startup in addition to those loaded in the AppDomain.</summary>
+        public IList<string> AssemblyNames { get; set; } = new List<string>();
+
+        #endregion
+
+        #region Nested classes
+
+        private class AttributedAssembly
+        {
+            internal Assembly Assembly { get; set; }
+            internal Type PluginAttributeType { get; set; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IEnumerable<Type> FindClassesOfType<T>(bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(typeof(T), onlyConcreteClasses);
+        }
+
+        public IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(assignTypeFrom, GetAssemblies(), onlyConcreteClasses);
+        }
+
+        public IEnumerable<Type> FindClassesOfType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            return FindClassesOfType(typeof(T), assemblies, onlyConcreteClasses);
+        }
+
+        public IEnumerable<Type> FindClassesOfType(Type assignTypeFrom, IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true)
+        {
+            var result = new List<Type>();
+            try
+            {
+                foreach (var a in assemblies)
+                {
+                    foreach (var t in a.DefinedTypes)
+                    {
+                        if ((assignTypeFrom.GetTypeInfo().IsAssignableFrom(t) || (assignTypeFrom.GetTypeInfo().IsGenericTypeDefinition && DoesTypeImplementOpenGeneric(t.DeclaringType, assignTypeFrom))) && !t.IsInterface)
+                        {
+                            if (onlyConcreteClasses)
+                            {
+                                if (t.IsClass && !t.IsAbstract)
+                                {
+                                    result.Add(t.DeclaringType);
+                                }
+                            }
+                            else
+                            {
+                                result.Add(t.DeclaringType);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                var msg = string.Join(Environment.NewLine, ex.LoaderExceptions.Select(e => e.Message));
+
+                var fail = new Exception(msg, ex);
+                Debug.WriteLine(fail.Message, fail);
+
+                throw fail;
+            }
+            return result;
+        }
+
+        /// <summary>Gets tne assemblies related to the current implementation.</summary>
+        /// <returns>A list of assemblies that should be loaded by the Nop factory.</returns>
+        public virtual IList<Assembly> GetAssemblies()
+        {
+            var addedAssemblyNames = new List<string>();
+            var assemblies = new List<Assembly>();
+
+            if (loadAppDomainAssemblies)
+                AddAssembliesInAppDomain(addedAssemblyNames, assemblies);
+            AddConfiguredAssemblies(addedAssemblyNames, assemblies);
+
+            return assemblies;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Iterates all assemblies in the AppDomain and if it's name matches the configured patterns add it to our list.
+        /// </summary>
+        /// <param name="addedAssemblyNames"></param>
+        /// <param name="assemblies"></param>
+        private void AddAssembliesInAppDomain(List<string> addedAssemblyNames, List<Assembly> assemblies)
+        {
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+            foreach (var library in dependencies)
+            {
+                if (Matches(library.Name) && !addedAssemblyNames.Contains(library.Name))
+                {
+                    var assembly = Assembly.Load(new AssemblyName(library.Name));
+                    assemblies.Add(assembly);
+                    addedAssemblyNames.Add(library.Name);
+                }
+            }
+        }
+        private bool IsCandidateCompilationLibrary(RuntimeLibrary compilationLibrary)
+        {
+            return compilationLibrary.Name == ("Specify")
+                || compilationLibrary.Dependencies.Any(d => d.Name.StartsWith("Specify"));
+        }
+
+        /// <summary>
+        /// Adds specificly configured assemblies.
+        /// </summary>
+        /// <param name="addedAssemblyNames"></param>
+        /// <param name="assemblies"></param>
+        protected virtual void AddConfiguredAssemblies(List<string> addedAssemblyNames, List<Assembly> assemblies)
+        {
+            foreach (string assemblyName in AssemblyNames)
+            {
+                Assembly assembly = Assembly.Load(new AssemblyName(assemblyName));
+                if (!addedAssemblyNames.Contains(assembly.FullName))
+                {
+                    assemblies.Add(assembly);
+                    addedAssemblyNames.Add(assembly.FullName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check if a dll is one of the shipped dlls that we know don't need to be investigated.
+        /// </summary>
+        /// <param name="assemblyFullName">
+        /// The name of the assembly to check.
+        /// </param>
+        /// <returns>
+        /// True if the assembly should be loaded into Nop.
+        /// </returns>
+        public virtual bool Matches(string assemblyFullName)
+        {
+            return !Matches(assemblyFullName, assemblySkipLoadingPattern)
+                   && Matches(assemblyFullName, assemblyRestrictToLoadingPattern);
+        }
+
+        /// <summary>
+        /// Check if a dll is one of the shipped dlls that we know don't need to be investigated.
+        /// </summary>
+        /// <param name="assemblyFullName">
+        /// The assembly name to match.
+        /// </param>
+        /// <param name="pattern">
+        /// The regular expression pattern to match against the assembly name.
+        /// </param>
+        /// <returns>
+        /// True if the pattern matches the assembly name.
+        /// </returns>
+        protected virtual bool Matches(string assemblyFullName, string pattern)
+        {
+            return Regex.IsMatch(assemblyFullName, pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        }
+
+        /// <summary>
+        /// Does type implement generic?
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="openGeneric"></param>
+        /// <returns></returns>
+        protected virtual bool DoesTypeImplementOpenGeneric(Type type, Type openGeneric)
+        {
+            try
+            {
+                var genericTypeDefinition = openGeneric.GetGenericTypeDefinition();
+                foreach (var implementedInterface in type.FindInterfaces((objType, objCriteria) => true, null))
+                {
+                    if (!implementedInterface.IsGenericType)
+                        continue;
+
+                    var isMatch = genericTypeDefinition.GetTypeInfo().IsAssignableFrom(implementedInterface.GetGenericTypeDefinition());
+                    return isMatch;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+
+}
